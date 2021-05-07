bc1d07d085a8dc36be6607fea3e62915edbdf008
Aziza  iliasova <miacorporation25@gmail.com>
2021-05-07 17:47:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+
+let element = document. getElementById('main');
+element.parentNode.removeChild(element);
+
+/* Enter the code to remove the main node element under this comment */
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
