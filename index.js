acfb0efbfcda057e269b370006fc497f3a7d98b6
Aziza  iliasova <miacorporation25@gmail.com>
2021-05-10 05:29:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 
+<<<<<<< HEAD
 let element = document. getElementById('main');
 element.parentNode.removeChild(element);
 
@@ -8,3 +9,16 @@ let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
 newHeader.innerHTML = 'YOUR-NAME is the champion';
 document.body.appendChild(newHeader);
+=======
+
+
+/* Enter the code to remove the main node element under this comment */
+
+/* Create your new element here and assign it to newHeader */
+const newHeader.nodeName = document.createElement("h1");
+const newHeader.id = document.createElement("victory");
+newHeader.innerHTML = "aziza is the champion";
+document.head.appendChild(element);
+
+
+>>>>>>> 4f1178317ae44917edd77a4fe220c8e9390f23cd
